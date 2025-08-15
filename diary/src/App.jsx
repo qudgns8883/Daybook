8c6b5a4bd4707e8d9a9fec0a661840a01621e08e
lbh <qudgns8882@naver.com>
2025-08-15 22:19:30 +0900

    feat: Implement DiaryList and page UI

--- diary/src/App.jsx
+++ diary/src/App.jsx
@@ -1,41 +1,87 @@
-import './App.css'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-import Home from './pages/Home';
-import New from './pages/New';
-import Diary from './pages/Diary';
-import Notfound from './pages/Notdound';
+import "./App.css";
+import { useReducer, useRef, createContext } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import Notfound from "./pages/Notdound";
 
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date("2025-08-15").getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date("2025-08-14").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2025-07-15").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  }
+];
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.data.id));
+    default:
+      return state;
+  }
+}
 
-// 1. "/" : 모든 일기를 조회하는 HOME페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary페이지
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
 function App() {
-  const nav = useNavigate();
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
 
-   const onClickButton = () => {
-    nav("/new");
-   }; 
+  //새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: { id: idRef.current++, createdDate, emotionId, content },
+    });
+  };
+  //기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({ type: "UPDATE", data: { id, createdDate, emotionId, content } });
+  };
+
+  //기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({ type: "DELETE", data: { id } });
+  };
+
   return (
     <>
-    <div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/new"}>new</Link>
-      <Link to={"/diary"}>diary</Link>
-    </div>
-    <button onClick={onClickButton}>
-      New 페이지로 이동
-    </button>
-    <Routes>
-    <Route path="/" element = {<Home />} />
-    <Route path="/new" element = {<New />} />
-    <Route path="/diary" element = {<Diary />} />
-    <Route path="*" element = {<Notfound />} />
-  </Routes>
+      <DiaryStateContext value={data}>
+        <DiaryDispatchContext.Provider value={{onCreate, onUpdate, onDelete}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext>
     </>
-  
-
   );
 }
 
-export default App
+export default App;
--- diary/src/pages/New.jsx
+++ diary/src/pages/New.jsx
@@ -1,5 +1,19 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+
 const New = () => {
-    return <div>New</div>
-}
+  const nav = useNavigate();
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로가기"} />}
+      />
+      <Editor />
+    </div>
+  );
+};
 
 export default New;
